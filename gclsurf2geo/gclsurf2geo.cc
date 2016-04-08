5e467a3b8530f497453329d169b0adb34afd5445
Xiaotao Yang <stcyang@gmail.com>
2016-04-08 14:20:44 -0400

    added options for other formats

--- gclsurf2geo/gclsurf2geo.cc
+++ gclsurf2geo/gclsurf2geo.cc
@@ -18,14 +18,22 @@ using namespace SEISPP;
 Changes:
 1. program name is changed from surf_vtk_converter to gclsurf2geo
 2. gclgrid information is read in from grid file directly without need of Antelope database.
+
+2016.04.06   XT Yang
+	1. add option to specify format.
 */
 
 void usage()
 {
-	cerr << "gclsurf2geo gridfile [-v] < infile"<<endl
+	cerr << "gclsurf2geo gridfile [-o outfile | -ix x_interval | -iy y_interval | "<<endl
+		 << "            -f infile_format_code | -v] < infile"<<endl
 	      << "* output is stdout"<<endl
-	      << "* gridfile shoule be 3d grid basename without extension."<<endl;
-
+	      << "* gridfile shoule be 3d grid basename without extension."<<endl
+	      << "* INPUT FILE FORMAT CODES:"<<endl
+	      << "    0 - gclgrid order (j, i, attribute) (default)"<<endl
+	      << "    1 - EarthVision points (ASCII) (x, y, attribute,"<<endl
+	      << "        column, row), 20 header lines. Requires -ix and/or -iy."<<endl
+	      << "    2 - CPS-3 lines (ASCII). Requires -ix and/or -iy."<<endl;
 	exit(-1);
 }
 bool SEISPP::SEISPP_verbose(false);
@@ -34,8 +42,12 @@ int main(int argc, char **argv)
 	int i,j;
 	ios::sync_with_stdio();
 	if(argc<2) usage();
+	
 	//string dbname(argv[1]);
-	string gridfile(argv[1]);
+	string gridfile(argv[1]),outfile("out.txt");
+	int formatcode(0);
+	bool y_interval_set(false),x_interval_set(false), savetofile(false);
+	double x_interval(-9999.9), y_interval(-9999.9);
 	//string fieldname(argv[3]);
 	ofstream outstrm;
 	for(i=2;i<argc;++i)
@@ -43,12 +55,58 @@ int main(int argc, char **argv)
 		string argstr=string(argv[i]);
 		if(argstr=="-v")
 			SEISPP_verbose=true;
+		else if(argstr=="-o")
+		{
+			++i;
+			if(i>=argc) usage();
+			outfile=string(argv[i]);
+			savetofile=true;
+		}
+		else if(argstr=="-f")
+		{
+			++i;
+			if(i>=argc) usage();
+			formatcode=atoi(argv[i]);
+		}
+		else if(argstr=="-ix")
+		{
+			++i;
+			if(i>=argc) usage();
+			x_interval=atof(argv[i]);
+			x_interval_set=true;
+		}
+		else if(argstr=="-iy")
+		{
+			++i;
+			if(i>=argc) usage();
+			y_interval=atof(argv[i]);
+			y_interval_set=true;
+		}
 		else
 		{
 			cerr << "Unknown argument = "<<argstr<<endl;
 			usage();
 		}
 	}
+	
+	if(formatcode!=0)
+	{
+		if(!y_interval_set && !x_interval_set)
+		{
+			cerr<<"**Error: wrong arguments. For format code 1 or 2,at least"<<endl
+				<<"         one of -ix and -iy needs to be specified!"<<endl;
+			exit(-1);
+		}
+		else if(x_interval_set && !y_interval_set)
+		{
+			y_interval = x_interval;
+		}
+		else if(y_interval_set && !x_interval_set)
+		{
+			x_interval = y_interval;
+		}
+	}
+	
 	try 
 	{
         /*
@@ -65,7 +123,7 @@ int main(int argc, char **argv)
 	    //GCLvectorfield3d g(gridfile);
         /* Now loop over grid.  */
         int i,j;
-        double xi,xj,depth,lat,lon;
+        double xi,xj,depth,lat,lon,x,y;
         double x1max,x2max,dxi,dxj;
         string temp_line,line;
         const double align_tolerance(0.01);
@@ -73,12 +131,49 @@ int main(int argc, char **argv)
         x1max=static_cast<double>(g.n1)-1;
         x2max=static_cast<double>(g.n2)-1;
         int nl(1);
+        
+        FILE * fhout;
+        if(savetofile) fhout=fopen(outfile.c_str(),"w");
+        
         while(getline(cin,temp_line))
         {
          	if(SEISPP_verbose) cerr<<"Working on line number: "<<nl<<endl;
+         	if(std::strcmp((char *)temp_line[0],"#")==0) continue;
+         	if(formatcode==2)
+         	{
+         		if(std::strcmp((char *)temp_line[0],"F")==0)
+         			continue;
+         		else if(std::strcmp((char *)temp_line[0],"-")==0 && 
+         				std::strcmp((char *)temp_line[1],">")==0)
+         		{
+         			fprintf(fhout,">\n");
+         			continue;
+         		}
+         	}
          	stringstream line(temp_line);
-           	line>>xj;
-           	line>>xi;
+           	switch(formatcode)
+           	{
+           		case 0:
+           			line>>xj;
+           			line>>xi;
+           			break;
+           		case 1:
+           			line>>x;
+           			line>>y;
+           			xj=static_cast<int>(y/y_interval);
+           			xi=static_cast<int>(x/x_interval);
+           			break;
+           		case 2:
+           			line>>x;
+           			line>>y;
+           			xj=static_cast<int>(y/y_interval);
+           			xi=static_cast<int>(x/x_interval);
+           			break;
+           		default:
+           			cerr<<"**ERROR: Unknow formatcode, could only be 0, 1, or 2."<<endl;
+           			exit(-1);
+           	}
+
            	line>>depth;
     		i=static_cast<int>(xi);
     		j=static_cast<int>(xj);
@@ -113,9 +208,14 @@ int main(int argc, char **argv)
     			lat=deg(lat);   
     			lon=deg(lon);
     		}
-    		cout<<lon<<"	"<<lat<<"	"<<depth<<endl;
+    		if(savetofile)
+    		{ fprintf(fhout,"%8.3f    %8.3f    %8.3f\n",lon,lat, depth);}
+    		else
+    		{cout<<lon<<"	"<<lat<<"	"<<depth<<endl;}
     		++nl;
         }
+        
+        if(savetofile) fclose(fhout);
 	}
 	catch (SeisppError& serr)
 	{
