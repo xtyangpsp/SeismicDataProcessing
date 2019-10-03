33e17046a247acddeebf58a6396a7876b3e50413
xtyangpsp <stcyang@gmail.com>
2019-10-02 23:40:53 -0400

    added spectrogram

--- smartstack/call_smartstack.m
+++ smartstack/call_smartstack.m
@@ -9,7 +9,8 @@ itmax=round((tmax - timeflag(1))/dt);
 
 
 [nsamp,ndata]=size(din);
-scale=0.75;
+scale=0.95;
+reftraceid=77; % the id of one trace with clear signal
 
 figure('position',[400 400 1050 650]); 
 subplot(2,2,[1,3]); hold on;
@@ -19,6 +20,9 @@ for i=1:ndata
 %     plot(timeflag(itmin:itmax),scale*abs(dtemp(itmin:itmax))/max(abs(dtemp(itmin:itmax))) + i,'r');
 %     drawnow;
 end
+
+h0=plot(timeflag(itmin:itmax),scale*din(itmin:itmax,reftraceid)/max(abs(din(itmin:itmax,reftraceid))) + reftraceid,'g','linewidth',2);
+
 dmean=nanmean(din,2);
 h1=plot(timeflag(itmin:itmax),scale*dmean(itmin:itmax)/max(abs(dmean(itmin:itmax))) + ndata + 2,'b','linewidth',1);
 
@@ -30,22 +34,24 @@ h3=plot(timeflag(itmin:itmax),scale*drobust(itmin:itmax)/max(abs(drobust(itmin:i
 xlim([tmin tmax])
 hold off;
 ylim([0 ndata+8])
-legend([h1,h2,h3],'mean','median','robust: entire','location','northwest');
+legend([h0,h1,h2,h3],'reference','Mean','Median','Robust: entire','location','southeast');
 xlabel('cross-correlation time (seconds)')
 box on;
 axis on;
 set(gca,'TickDir','out','fontsize',14)
 
 subplot(2,2,2); hold on;
-h1=plot(timeflag(itmin:itmax),scale*dmean(itmin:itmax)/max(abs(dmean(itmin:itmax))) + 2,'b','linewidth',1);
+h0=plot(timeflag(itmin:itmax),scale*din(itmin:itmax,reftraceid)/max(abs(din(itmin:itmax,reftraceid))) + 2,'g','linewidth',2);
 
-h2=plot(timeflag(itmin:itmax),scale*dmedian(itmin:itmax)/max(abs(dmedian(itmin:itmax))) + 4,'r','linewidth',1);
+h1=plot(timeflag(itmin:itmax),scale*dmean(itmin:itmax)/max(abs(dmean(itmin:itmax))) +4,'b','linewidth',1);
 
-h3=plot(timeflag(itmin:itmax),scale*drobust(itmin:itmax)/max(abs(drobust(itmin:itmax))) + 6,'m','linewidth',1);
+h2=plot(timeflag(itmin:itmax),scale*dmedian(itmin:itmax)/max(abs(dmedian(itmin:itmax))) + 6,'r','linewidth',1);
+
+h3=plot(timeflag(itmin:itmax),scale*drobust(itmin:itmax)/max(abs(drobust(itmin:itmax))) + 8,'m','linewidth',1);
 xlim([tmin tmax])
 hold off;
-ylim([0 8])
-legend([h1,h2,h3],'mean','median','robust: entire','location','northwest');
+ylim([0 10])
+% legend([h0,h1,h2,h3],'reference','Mean','Median','Robust: entire','location','northwest');
 xlabel('cross-correlation time (seconds)')
 box on;
 axis on;
@@ -75,8 +81,42 @@ drawnow;
 xlim([tmin tmax])
 hold off;
 ylim([0 8])
-legend('rubust: entire','robust: 0-200','robust: 50-150','location','northwest');
+legend('Rubust: entire','Robust: 0-200','Robust: 50-150','location','northwest');
 xlabel('cross-correlation time (seconds)')
 box on;
 axis on;
 set(gca,'TickDir','out','fontsize',14)
+saveas(gca,'smartstack_example.png','png')
+
+%% plot spectrum of the traces
+myylim=[0.01 2.5];
+figure('position',[400 400 950 600])
+subplot(2,2,1)
+dref=din(:,reftraceid)/max(abs(din(:,reftraceid)));
+spectrogram(dref,300,150,600,5,'yaxis');ax=gca;ax.YScale = 'log';
+ylim(myylim)
+title('Reference trace');
+set(gca,'FontSize',14)
+
+subplot(2,2,2)
+dmeantemp=dmean/max(abs(dmean));
+spectrogram(dmean,300,150,300,5,'yaxis');ax=gca;ax.YScale = 'log';
+title('Mean stack');
+ylim(myylim)
+set(gca,'FontSize',14)
+
+subplot(2,2,3)
+dmediantemp=dmedian/max(abs(dmedian));
+spectrogram(dmediantemp,300,150,300,5,'yaxis');ax=gca;ax.YScale = 'log';
+title('Median');
+ylim(myylim)
+set(gca,'FontSize',14)
+
+subplot(2,2,4)
+drobusttemp=drobust/max(abs(drobust));
+spectrogram(drobusttemp,300,150,600,5,'yaxis');ax=gca;ax.YScale = 'log';
+title('Robust stack');
+ylim(myylim)
+set(gca,'FontSize',14)
+
+saveas(gca,'smartstack_example_spectrograms.png','png')
